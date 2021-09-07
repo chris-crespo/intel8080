be7f2db19896676654d51435a4fbb150d3d3b251
krs98 <christian.crespoo98@gmail.com>
2021-09-07 09:21:02 +0200

    Add function to load tests

--- include/memory.h
+++ include/memory.h
@@ -4,6 +4,7 @@
 #include "types.h"
 
 void load_rom(const char *rom);
+void load_test(const char *test);
 
 u8 memory_read(u16 addr);
 void memory_write(u16 addr, u8 value);
--- src/memory.c
+++ src/memory.c
@@ -3,7 +3,7 @@
 
 #include "memory.h"
 
-static u8 memory[0x8000];
+static u8 memory[0x10000];
 
 static File *open_file(const char *filename) {
     File *file = fopen(filename, "rb");
@@ -29,6 +29,16 @@ void load_rom(const char *rom) {
     load_part(0x0800, rom, 'g');
     load_part(0x1000, rom, 'f');
     load_part(0x1800, rom, 'e');
+}
+
+void load_test(const char *test) {
+    File *file = open_file(test);
+    fread(&memory[0x100], 1, 0x8000, file);
+    fclose(file);
+
+    memory[0x5] = 0xd3;
+    memory[0x6] = 0x1;
+    memory[0x7] = 0xc9;
 }
 
 u8 memory_read(u16 addr) {
