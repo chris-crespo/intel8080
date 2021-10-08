52d660fb9b55996f9e4b7f88c8d6cb0f5c60e3f5
krs98 <christian.crespoo98@gmail.com>
2021-10-08 15:33:30 +0200

    Inject instructions to handle output

--- src/test.c
+++ src/test.c
@@ -18,8 +18,15 @@ static void test(const char *filename) {
     CPU cpu;
     cpu_init(&cpu, 0, 0);
 
-    load_test(cpu.memory, filename);
+    load_test(&cpu.memory[0x100], filename);
     cpu.pc = 0x100;
+
+    cpu.memory[0x0] = 0xd3;
+    cpu.memory[0x1] = 0x00;
+
+    cpu.memory[0x5] = 0xd3;
+    cpu.memory[0x6] = 0x01;
+    cpu.memory[0x7] = 0xc9;
 
     while (1) {
         cpu_execute(&cpu, 0);
