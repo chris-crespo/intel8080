bf5fffc3e3c281d736f63408cdc2e26112425a87
krs98 <christian.crespoo98@gmail.com>
2021-10-26 17:43:41 +0200

    Add screen to main

--- invaders/main.c
+++ invaders/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL.h>
 
 #include "cpu.h"
+#include "screen.h"
 
 static File *open_rom(void) {
     File *file = fopen("roms/invaders/invaders", "rb");
@@ -19,12 +21,61 @@ static void load_rom(u8 *memory) {
     fclose(rom);
 }
 
+static void in(CPU *cpu) {
+    fprintf(stderr, "Out not implemented.\n");
+}
+
+static void out(CPU *cpu, u8 port) {
+    switch (port) {
+        case 3: {
+            printf("Looks like this plays some sounds.\n");
+            printf("Input byte? %02x\n", cpu->regs.c);
+            break;
+        }
+        case 5: {
+            printf("More sounds?\n");
+            printf("Is this the byte? %02x\n", cpu->regs.a);
+            break;
+        }
+        case 6: {
+            printf("What's this for?\n");
+            printf("Input byte? %02x\n", cpu->regs.a);
+            break;
+        }
+        default: {
+            fprintf(stderr, "Port %d not handled.\n", port);
+            exit(1);
+        }
+    }
+}
+
+static inline bool window_close(SDL_Event event) {
+    return event.type == SDL_WINDOWEVENT
+        && event.window.event == SDL_WINDOWEVENT_CLOSE;
+}
+
+static u32 last_tick = 0;
+
 int main(void) {
     CPU cpu;
-    cpu_init(&cpu, 0, 0);
+
+    cpu_init(&cpu, in, out);
     load_rom(cpu.memory);
 
+    screen_init();
+
     while (1) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (window_close(event))
+                screen_quit();
+        }
+
+        if (SDL_GetTicks() - last_tick > 10) {
+            screen_draw(&cpu.memory[0x2400]);
+            last_tick = SDL_GetTicks();
+        }
+
         cpu_execute(&cpu);
     }
 }
